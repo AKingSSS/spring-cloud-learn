712b026f97bd0d554d4ed0ef5a8499cec29d9215
yangkang <2276306206@qq.com>
2020-12-12 23:24:23 +0800

    eureka 单机&集群 provider 单机&集群注册

---
+++ spring-cloud-eureka-7001/src/main/java/com/aking/cloud/Eurekaserverapplication_7001.java
@@ -0,0 +1,22 @@
+package com.aking.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * <p>
+ *
+ * </p>
+ *
+ * @author yk
+ * @date 2020/12/12
+ */
+@SpringBootApplication
+@EnableEurekaServer  // 服务端启动类，接受别人注册
+public class Eurekaserverapplication_7001 {
+    public static void main(String[] args) {
+        SpringApplication.run(Eurekaserverapplication_7001.class, args);
+    }
+
+}
--- spring-cloud-provider-8081/src/main/java/com/aking/cloud/ProviderApplication.java
+++ spring-cloud-provider-8081/src/main/java/com/aking/cloud/ProviderApplication.java
@@ -3,6 +3,7 @@ package com.aking.cloud;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * <p>
@@ -15,6 +16,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 // 启动类
 @SpringBootApplication
 @MapperScan("com.aking.cloud.dao")
+@EnableEurekaClient   // 在服务启动后自动注册到 eureka 服务中
 public class ProviderApplication {
     public static void main(String[] args) {
         SpringApplication.run(ProviderApplication.class, args);
